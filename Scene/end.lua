e81038ae9a2db03e3735545c2a6ff36919e6256f
HeoJiye <heoze1031@gmail.com>
2022-11-13 16:39:34 +0900

    공룡 게임

---
+++ Scene/end.lua
@@ -0,0 +1,61 @@
+-----------------------------------------------------------------------------------------
+--
+-- end.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+function scene:create( event )
+	local sceneGroup = self.view
+	local background = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
+
+	local getScore = 0
+	local scoreUI = display.newText(getScore, display.contentWidth/2, display.contentHeight/2)
+    scoreUI:setFillColor(0)
+    scoreUI.size = 100
+
+	function background:tap()
+		composer.gotoScene("Scene.start")
+	end
+
+    background:addEventListener("tap", background)
+
+    sceneGroup:insert( background )
+    sceneGroup:insert( scoreUI )
+end
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if phase == "will" then
+
+	elseif phase == "did" then
+
+	end	
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if event.phase == "will" then
+
+	elseif phase == "did" then
+
+	end
+end
+
+function scene:destroy( event )
+	local sceneGroup = self.view
+
+end
+
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+return scene
